986d238d118d57e670dfb7820d763f293bc7dbee
aldtec <aldtec@outlook.com>
2020-03-23 01:05:20 -0300

    first commit

--- appescola/urls.py
+++ appescola/urls.py
@@ -1,4 +1,4 @@
-"""dev URL Configuration
+"""project_name URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.views.generic.base import TemplateView
+
+# Usados para tentar static files
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
-]
+    path('accounts/', include('django.contrib.auth.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) #static files
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dev.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appescola.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
